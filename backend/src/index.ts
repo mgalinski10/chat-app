2da8651eebf3881ea61d62701e28bfed6e18a81a
Michal <michalg359@gmail.com>
2025-05-12 21:23:20 +0200

    test husky

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
