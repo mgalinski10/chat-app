91cdb238274d675afb852fad8daa9d936855e0e7
Michal <michalg359@gmail.com>
2025-05-17 16:42:20 +0200

    feat: add ProfileCard

--- web/app/(layout)/messages/page.tsx
+++ web/app/(layout)/messages/page.tsx
@@ -4,6 +4,7 @@ import Spinner from '@/components/Spinner/Spinner';
 import { useUser } from '@/hooks/useUser';
 import React from 'react';
 import Card from '@/components/Card/Card';
+import ProfileCard from '@/components/ProfileCard/ProfileCard';
 
 const MessagesPage = () => {
   const { user } = useUser();
@@ -13,12 +14,14 @@ const MessagesPage = () => {
   }
 
   return (
-    <div className="grid grid-cols-3 gap-5 w-full h-full">
-      <div className="col-span-2">
+    <div className="grid grid-cols-4 gap-5 w-full h-full">
+      <div className="col-span-3">
         <Card>Welcome, {user.firstName}</Card>
       </div>
       <div className="col-span-1">
-        <Card>Profile information</Card>
+        <Card>
+          <ProfileCard />
+        </Card>
       </div>
     </div>
   );
