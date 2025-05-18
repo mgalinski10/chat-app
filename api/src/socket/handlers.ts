e9772d4a25f3a6c8dd0bd1e7ee20990e46b69e85
Michal <michalg359@gmail.com>
2025-05-18 14:47:06 +0200

    fix: function to many arguments

--- api/src/socket/handlers.ts
+++ api/src/socket/handlers.ts
@@ -1,6 +1,6 @@
-import { Socket } from 'socket.io';
-
-export const testSocketHandlers = (socket: Socket) => {
+import { Server, Socket } from 'socket.io';
+// eslint-disable-next-line
+export const testSocketHandlers = (socket: Socket, io: Server) => {
   socket.on('test', (data) => {
     console.log('📨 test:', data, 'from:', socket.data.user);
   });
