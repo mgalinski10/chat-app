38e92e195c61b0b71ba836859fe0fa5c396b01fb
Michal <michalg359@gmail.com>
2025-06-02 12:16:11 +0200

    feat: add notification UI concept

--- web/app/(layout)/notifications/page.tsx
+++ web/app/(layout)/notifications/page.tsx
@@ -1,12 +1,41 @@
 import Card from '@/components/Card/Card';
 import NotificationList from '@/components/NotificationList/NotificationList';
+import { IoIosInformationCircleOutline } from 'react-icons/io';
 
 const NotificationPage = () => {
+  const mock = [
+    {
+      id: 1,
+      fromUser: {
+        firstName: 'Jan',
+        lastName: 'Kowalski',
+      },
+      createdAt: new Date().toISOString(),
+      read: false,
+    },
+    {
+      id: 2,
+      fromUser: {
+        firstName: 'Anna',
+        lastName: 'Nowak',
+      },
+      createdAt: new Date(Date.now() - 2 * 60 * 60 * 1000).toISOString(),
+      read: true,
+    },
+  ];
+
   return (
     <Card>
       <p className="text-lg font-semibold mb-5">Notifications</p>
       <div>
-        <NotificationList />
+        {mock.length === 0 ? (
+          <div className="w-full flex items-center justify-center space-x-2 text-gray-600">
+            <IoIosInformationCircleOutline />
+            <span>No notifications to display at the moment.</span>
+          </div>
+        ) : (
+          <NotificationList notifications={mock} />
+        )}
       </div>
     </Card>
   );
--- web/components/NotificationList/NotificationList.tsx
+++ web/components/NotificationList/NotificationList.tsx
@@ -1,55 +1,58 @@
 'use client';
+import Link from 'next/link';
 
-const NotificationList = () => {
-  const mockNotifications = [
-    {
-      id: '1',
-      fromUser: {
-        firstName: 'Jan',
-        lastName: 'Kowalski',
-      },
-      createdAt: new Date().toISOString(),
-      read: false,
-    },
-    {
-      id: '2',
-      fromUser: {
-        firstName: 'Anna',
-        lastName: 'Nowak',
-      },
-      createdAt: new Date(Date.now() - 2 * 60 * 60 * 1000).toISOString(),
-      read: true,
-    },
-  ];
+type Notification = {
+  id: number;
+  fromUser: {
+    firstName: string;
+    lastName: string;
+  };
+  createdAt: string;
+  read: boolean;
+};
 
+interface NotificationListProps {
+  notifications: Notification[];
+}
+
+const NotificationList: React.FC<NotificationListProps> = ({
+  notifications,
+}) => {
   return (
     <div className="flex flex-col gap-4">
-      {mockNotifications.map((n) => (
+      {notifications.map((notification) => (
         <div
-          key={n.id}
-          className={`flex items-center justify-between p-4 rounded-xl border `}
+          key={notification.id}
+          className="flex items-center justify-between p-4 rounded-xl bg-white shadow-sm"
         >
           <div className="flex items-center gap-4">
             <img
-              src={`https://api.dicebear.com/7.x/thumbs/svg?seed=${n.id}`}
-              className="w-12 h-12 rounded-full object-cover mb-3"
+              src={`https://api.dicebear.com/7.x/thumbs/svg?seed=${notification.id}`}
+              className="w-12 h-12 rounded-full object-cover"
             />
             <div>
               <p className="font-medium">
-                {n.fromUser.firstName} {n.fromUser.lastName}
+                {notification.fromUser.firstName}{' '}
+                {notification.fromUser.lastName}{' '}
+                <span className="text-gray-400">
+                  has sent you a contact request.
+                </span>
               </p>
-              <p className="text-xs text-gray-500">14:00</p>
             </div>
           </div>
-          <div className="flex items-center gap-3">
-            <span
-              className={`text-xs ${n.read ? 'text-gray-400' : 'text-blue-700'}`}
-            >
-              {n.read ? 'Read' : 'Unread'}
-            </span>
-            <button className="text-sm bg-blue-600 hover:bg-blue-700 text-white px-3 py-1 rounded-md">
-              Check
-            </button>
+
+          <p className=" text-gray-400">1 hour ago</p>
+          <span
+            className={`${notification.read ? 'text-gray-400' : 'text-blue-700'}`}
+          >
+            {notification.read ? 'Read' : 'Unread'}
+          </span>
+          <div className="flex items-center gap-4">
+            <Link href="/contacts/requests">
+              <button className="font-bold  bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-md hover:cursor-pointer">
+                Check
+              </button>
+            </Link>
           </div>
         </div>
       ))}
