5146e18feeddec9c378bcd2c6189479bfc18982c
Michal <michalg359@gmail.com>
2025-06-02 16:48:01 +0200

    feat: add logout button

--- web/components/Sidebar/Sidebar.tsx
+++ web/components/Sidebar/Sidebar.tsx
@@ -2,6 +2,8 @@
 
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { FiLogOut } from 'react-icons/fi';
 import { FiMessageSquare, FiBell, FiUsers, FiSettings } from 'react-icons/fi';
 
 const sidebarItems: Item[] = [
@@ -56,11 +58,32 @@ const SidebarItem: React.FC<SidebarItemProps> = ({ item }) => {
 };
 
 const Sidebar = () => {
+  const router = useRouter();
+  const handleLogout = async () => {
+    try {
+      await fetch('http://localhost:5000/auth/logout', {
+        method: 'POST',
+        credentials: 'include',
+      });
+
+      router.push('/login'); // albo '/' zależnie gdzie ma lecieć
+    } catch (err) {
+      console.error('Logout failed', err);
+    }
+  };
   return (
     <ul className="flex flex-col justify-center gap-30 w-32">
       {sidebarItems.map((item) => {
         return <SidebarItem key={item.name} item={item} />;
       })}
+      <li
+        onClick={handleLogout}
+        className="flex h-10 justify-center border-l-4 cursor-pointer transition-colors duration-300 border-transparent hover:border-white "
+      >
+        <button className="flex items-center justify-center w-full hover:cursor-pointer ">
+          <FiLogOut className="text-white text-2xl" />
+        </button>
+      </li>
     </ul>
   );
 };
